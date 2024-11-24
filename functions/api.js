d6948a72b68fc9ab1798a3d0254a9c49e6a1274d
. <barcalover016@gmail.com>
2024-11-24 22:22:12 +0100

    initial commit

---
+++ functions/api.js
@@ -0,0 +1,28 @@
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("../schema/schema.js");
+const connectDB = require("../config/db.js");
+const serverless = require("serverless-http");
+
+const port = process.env.PORT || 3030;
+const app = express();
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === "dev" })
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to the API" });
+});
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server running on port: ${port}`);
+});
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
